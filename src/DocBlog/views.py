8089036524e8ed92c4238f43e2d18d14aba4fc4c
Phetsana Phommarinh <phetsana@kannelle.io>
2023-05-04 16:24:09 +0200

    test

---
+++ src/DocBlog/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from datetime import datetime
+
+
+def index(request):
+    date = datetime.today()    
+    return render(request, "DocBlog/index.html", context={"date": date})
